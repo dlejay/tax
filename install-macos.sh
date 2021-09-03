148efa76bff9ae3cb78cbeaa437982f80b3e31bd
dlejay <39823850+dlejay@users.noreply.github.com>
2021-09-03 14:25:59 +0900

    Make rm silent

--- install-macos.sh
+++ install-macos.sh
@@ -1,6 +1,6 @@
 cd $TMPDIR
 
-rm -r TaX
+rm -rf TaX
 
 curl -sLO https://raw.githubusercontent.com/dlejay/tax/trunk/tax.profile
 curl -sL https://raw.githubusercontent.com/dlejay/tax/trunk/install-base.sh | sh
@@ -12,4 +12,4 @@ curl -sLO https://raw.githubusercontent.com/dlejay/tax/trunk/packages.txt
 ./tlmgr install $(cat packages.txt | tr '\n' ' ')
 
 rm packages.txt
-mv ../../TaX $HOME/Library
+mv ../../../TaX $HOME/Library
