52ce565ca759d9533ac0e9d9f260df638cf7d735
dlejay <39823850+dlejay@users.noreply.github.com>
2021-09-03 14:22:40 +0900

    Add install script

---
+++ install-macos.sh
@@ -0,0 +1,15 @@
+cd $TMPDIR
+
+rm -r TaX
+
+curl -sLO https://raw.githubusercontent.com/dlejay/tax/trunk/tax.profile
+curl -sL https://raw.githubusercontent.com/dlejay/tax/trunk/install-base.sh | sh
+
+rm tax.profile
+
+cd TaX/bin/*
+curl -sLO https://raw.githubusercontent.com/dlejay/tax/trunk/packages.txt
+./tlmgr install $(cat packages.txt | tr '\n' ' ')
+
+rm packages.txt
+mv ../../TaX $HOME/Library
